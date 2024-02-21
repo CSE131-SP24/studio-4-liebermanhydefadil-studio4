ba6e8c30a0937c3ce10a7a185966bbacbe732449
nicklieberman <nicklieberman@localhost>
2024-02-21 14:14:33 -0600

    Mike

--- src/studio4/Flag.java
+++ src/studio4/Flag.java
@@ -7,5 +7,28 @@ import edu.princeton.cs.introcs.StdDraw;
 public class Flag {
 	public static void main(String[] args) {
 		
+	Color body = new Color(163, 216, 109);
+	StdDraw.setPenColor(body);
+	StdDraw.filledCircle(0.5, 0.5, 0.45);
+	
+	StdDraw.setPenColor(Color.WHITE);
+	StdDraw.filledEllipse(0.5, 0.65, 0.2, 0.175);
+
+	Color eye = new Color(20, 130, 83);
+	StdDraw.setPenColor(eye);
+	StdDraw.filledCircle(0.5, 0.65, 0.11);
+	
+	StdDraw.setPenColor(Color.black);
+	StdDraw.filledCircle(0.5, 0.65, 0.06);
+
+	StdDraw.setPenColor(Color.darkGray);
+	StdDraw.filledEllipse(0.5, 0.302, 0.30, 0.17);
+
+	StdDraw.setPenColor(body);
+	StdDraw.filledEllipse(0.5, 0.375, 0.30, 0.10);
+
+
+
+		
 	}
 }
